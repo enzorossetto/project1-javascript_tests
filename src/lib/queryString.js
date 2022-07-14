19f28b794dd489d1cace5b9736e24f74ff07498f
Enzo Rossetto <enzojmrossetto@gmail.com>
2022-07-13 22:18:24 -0300

    setting up prettier

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,5 +1,5 @@
-module.exports.queryString = (obj) => {
-  const entries = Object.entries(obj).map((item) => `${item[0]}=${item[1]}`);
+module.exports.queryString = obj => {
+  const entries = Object.entries(obj).map(item => `${item[0]}=${item[1]}`);
 
   return entries.join('&');
 };
