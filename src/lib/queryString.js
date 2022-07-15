1e341e62c7289b28da6efde2aa641cb0322acf27
Enzo Rossetto <enzojmrossetto@gmail.com>
2022-07-15 14:53:48 -0300

    Add method parse and tests for it

--- src/lib/queryString.js
+++ src/lib/queryString.js
@@ -1,4 +1,23 @@
+const keyValueToString = ([key, value]) => {
+  if (typeof value === 'object' && !Array.isArray(value)) {
+    throw new Error('Please check your params');
+  }
+
+  return `${key}=${value}`;
+};
+
 module.exports.queryString = obj =>
-  Object.entries(obj)
-    .map(([key, value]) => `${key}=${value}`)
-    .join('&');
+  Object.entries(obj).map(keyValueToString).join('&');
+
+module.exports.parse = str =>
+  Object.fromEntries(
+    str.split('&').map(item => {
+      let [key, value] = item.split('=');
+
+      if (value.includes(',')) {
+        value = value.split(',');
+      }
+
+      return [key, value];
+    }),
+  );
--- src/lib/queryString.spec.js
+++ src/lib/queryString.spec.js
@@ -1,4 +1,4 @@
-const { queryString } = require('./queryString');
+const { queryString, parse } = require('./queryString');
 
 describe('Object to query string', () => {
   it('should create a valid query string when an object is provided', () => {
@@ -9,6 +9,55 @@ describe('Object to query string', () => {
 
     expect(queryString(obj)).toBe('name=Enzo&profession=developer');
   });
+
+  it('should create a valid query string even when an array is passed as value', () => {
+    const obj = {
+      name: 'Enzo',
+      abilities: ['JS', 'TDD'],
+    };
+
+    expect(queryString(obj)).toBe('name=Enzo&abilities=JS,TDD');
+  });
+
+  it('should throw an error when an object is passed as value', () => {
+    const obj = {
+      name: 'Enzo',
+      abilities: {
+        fist: 'JS',
+        second: 'TDD',
+      },
+    };
+
+    expect(() => {
+      queryString(obj);
+    }).toThrowError();
+  });
 });
 
-describe('Query string to object', () => {});
+describe('Query string to object', () => {
+  it('should convert a query string to object', () => {
+    const qs = 'name=Enzo&profession=developer';
+
+    expect(parse(qs)).toEqual({
+      name: 'Enzo',
+      profession: 'developer',
+    });
+  });
+
+  it('should convert a query string of a single key-value pair to object', () => {
+    const qs = 'name=Enzo';
+
+    expect(parse(qs)).toEqual({
+      name: 'Enzo',
+    });
+  });
+
+  it('should convert a query string to an object taking care of comma separated values', () => {
+    const qs = 'name=Enzo&abilities=JS,TDD';
+
+    expect(parse(qs)).toEqual({
+      name: 'Enzo',
+      abilities: ['JS', 'TDD'],
+    });
+  });
+});
